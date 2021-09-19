8eee6f6d7c07ba66a0dfea51b84c327c562ba54b
phonqha <phongdz254@gmail.com>
2021-09-19 20:54:34 +0700

    2.AboutUs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import AboutSection from "./components/AboutSection";
+import AboutUs from "./pages/AboutUs";
+
 function App() {
     return (
         <div className="App">
-            <h1>Film</h1>
+            <AboutUs />
         </div>
     );
 }
