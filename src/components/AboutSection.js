6967276b114c0003fe2bb719245ebe7a8e2d3ab5
phonqha <phongdz254@gmail.com>
2021-09-22 15:10:02 +0700

    12.Framer Motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,24 +2,44 @@ import React from "react";
 import home1 from "../img/home1.png";
 // Styled
 import { About, Description, Image, Hide } from "../styles";
+//frame motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+    const titleAnim = {
+        hidden: { opacity: 0 },
+        show: { opacity: 1, transition: { duration: 2 } },
+    };
+    const container = {
+        hidden: { x: 100 },
+        show: {
+            x: 0,
+            transition: { duration: 0.75, ease: "easeOut", staggerChildren: 1 },
+        },
+    };
     return (
         <About>
             <Description>
-                <div className="title">
+                <motion.div
+                    variants={container}
+                    animate="show"
+                    initial="hidden"
+                    className="title"
+                >
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnim}>
+                            We work to make
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>
+                        <motion.h2 variants={titleAnim}>
                             your <span>dreams</span> come
-                        </h2>
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>
                     Contact us for any photography or videography ideas that you
                     have. We have professionals with amazing skills to help you
